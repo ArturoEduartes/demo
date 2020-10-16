42654d0d9ec12647c4a1bbe6ced3a04fd0d4be9a
MyUser <ArturoEduartes@users.noreply.github.com>
2020-10-15 23:02:27 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,13 @@
+// sample inspired in https://spring.io/guides/gs/accessing-data-mysql/
+
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/demo/User.kt
@@ -0,0 +1,17 @@
+package com.example.demo
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.SequenceGenerator
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+@Entity // This tells Hibernate to make a table out of this class
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int? = null
+    var name: String? = null
+    var email: String? = null
+}
